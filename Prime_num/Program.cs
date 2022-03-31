b3affc605addd9790e686e1788dcc7a48714b952
Admin <Admin@DESKTOP-Q9C1DBG>
2022-03-31 19:40:07 +0530

    Add project files.

---
+++ Prime_num/Program.cs
@@ -0,0 +1,51 @@
+﻿class prime 
+{
+    static void Main(string[] args)
+    {
+        int c = 0; int n1; int i; int r;
+        System.Console.WriteLine("Enter any number");
+        n1 = int.Parse(System.Console.ReadLine());
+        if (n1 == 0 || n1 == 1)
+        {
+            System.Console.WriteLine("Not Prime Number");
+
+        }
+        else
+        {
+            for (i = 2; i <= n1 / 2; i++)
+            {
+                r = n1 % i;
+                if (r == 0)
+                    c++;
+            }
+                if (c != 2)
+                {
+                    System.Console.WriteLine("This is Prime Number");
+                }
+            else
+            {
+                System.Console.WriteLine("Not Prime Number");
+            }
+            
+                
+
+
+
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+
+}
